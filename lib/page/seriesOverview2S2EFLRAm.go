3310cee0a3bbe5b934ce71ead0317e939a32c03e
George Smith <litesoft@gmail.com>
2016-10-12 18:55:52 -0700

    Updated File...

--- lib/page/seriesOverview2S2EFLRAm.go
+++ lib/page/seriesOverview2S2EFLRAm.go
@@ -2,18 +2,16 @@ package page
 
 import (
 	html "svc-wiki-showepisodes/lib/htmlplus"
-	"fmt"
 )
 
 func init() {
-	addSeriesOverview(process2S2EFLAm,
+	addSeriesOverview(process2S2EFLRAm, "2S2EFLRAm",
 		html.HeaderRow{"Season", "Season", "Episodes", "Episodes", "Originally aired", "Originally aired", "Nielsen ratings", "Nielsen ratings"},
 		html.HeaderRow{"Season", "Season", "Episodes", "Episodes", "First aired" /**/, "Last aired" /* */, "Rank" /* . . .*/, "Average viewers|||(in millions)"})
 }
 
-func process2S2EFLAm(pTable *html.Table) ([]*season, error) {
-	fmt.Println("2S2EFLAm")
-	return populateFromSOT(pTable,newSOTrowProcessors().
-		add(newSimpleSOTrowProcessor(sSOTcellIgnored, sSTOcellSeasonNumber, sSTOcellEpisodeCount.colspan(2), sSTOcellFirstAirDate, sSTOcellLastAirDate, sSOTcellIgnored, sSOTcellIgnored)))
+func process2S2EFLRAm(pTable *html.Table) ([]*season, error) {
+	return populateFromSOT(pTable, newSOTrowProcessors().
+			add(newSimpleSOTrowProcessor(sSOTcellIgnored, sSTOcellSeasonNumber, sSTOcellEpisodeCount.colspan(2), sSTOcellFirstAirDate, sSTOcellLastAirDate, sSOTcellIgnored, sSOTcellIgnored)))
 }
 
